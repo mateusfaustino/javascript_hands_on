a51ccccc3cc37cfdbdb3c8c453f84eeb95b4b07d
Mateus Lucena <mateusfaustino2017@gmail.com>
2024-03-07 22:26:14 -0300

    Update 03.js

--- arrays/03.js
+++ arrays/03.js
@@ -10,8 +10,10 @@
 // entre 3 e 4 respostas positivas como "Cúmplice"; e se responder positivamente a 5 questões, deve ser classificada como "Assassino". 
 // Caso contrário, será classificada como "Inocente.
 
-// Passo 1: Crie um vetor para armazenar as respostas
-let respostas = [];
+let contador = 0
+let respostasSim = 0
+let statusPessoa = ''
+
 let perguntas = [
     "Telefonou para a vítima?",
     "Esteve no local do crime?",
@@ -19,25 +21,33 @@ let perguntas = [
     "Devia para a vítima?",
     "Já trabalhou com a vítima?"
 ]
-// Passo 2: Use um loop para fazer as 5 perguntas
-for (let i = 1; i <= 5; i++) {
-    // Leitura da resposta (pode ser substituído por lógica de leitura)
-    let resposta = prompt(perguntas[i]);
 
-    // Armazene a resposta no vetor
-    respostas.push(resposta.toLowerCase()); // Converta para minúsculas para facilitar a comparação
+while (true) {
+    if(contador>=perguntas.length){
+        break
+    }
+    
+    let resposta = prompt("Responda sim ou nao \n"+perguntas[contador])
+    
+    if(resposta!='sim' && resposta!='nao'){
+        alert('Responda "sim" ou "nao"' )
+        continue
+    }
+
+    if(resposta=="sim"){
+        respostasSim++
+    }
+
+    contador++
+}
+if(respostasSim==2){
+    statusPessoa = "Suspeito"
+}else if(respostasSim==3 || respostasSim==4){
+    statusPessoa = "Cúplice"
+}else if(respostasSim>=5){
+    statusPessoa = "Assasino"
+}else{
+    statusPessoa = "Inocente"
 }
 
-// Passo 3: Conte o número de respostas positivas
-let respostasPositivas = respostas.filter(resposta => resposta === 'sim').length;
-
-// Passo 4: Emita uma classificação com base nas respostas
-if (respostasPositivas === 5) {
-    console.log("Classificação: Assassino");
-} else if (respostasPositivas >= 3) {
-    console.log("Classificação: Cúmplice");
-} else if (respostasPositivas >= 2) {
-    console.log("Classificação: Suspeita");
-} else {
-    console.log("Classificação: Inocente");
-}
+alert("Você é "+statusPessoa)
